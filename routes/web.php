ea602ded2206e57c08a66aa166fdb672fbfb0c28
aaronsleepy <aaron@kmong.com>
2021-12-26 19:00:17 +0900

    task: routing 예제 추가

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+// route name 지정
+Route::get('/test', [
+  'as' => 'test',
+  function() {
+    return 'test';
+  }
+]);
+
+Route::get('/test2', function () {
+  info('Redirecting from test2 to test...'.'\n');
+  return redirect(route('test'));
+});
+
+// 영어,숫자로 2자리 이상 5자리 이하로 구성된 경로만 처리
+Route::get('/{foo?}', function ($foo = 'bar') {
+  return "{$foo}";
+})->where('foo', '[0-9a-zA-Z]{2,5}');
