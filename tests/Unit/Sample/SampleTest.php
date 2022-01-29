ab0433efda7dbf6e2c1d857ee7557f75b0b7f891
aaronsleepy <aaron@kmong.com>
2022-01-29 20:54:51 +0900

    feature: 샘플 테스트 추가

---
+++ tests/Unit/Sample/SampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\Sample;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
